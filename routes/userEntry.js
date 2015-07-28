c41734e245062afd65d116965de22b3ee3c9dd8d
Rohtash <rohtashpoonia644@gmail.com>
2015-07-29 00:17:24 +0530

    mongodb post scenario

--- routes/userEntry.js
+++ routes/userEntry.js
@@ -1,11 +1,13 @@
 var User = require('../models/minions').User;
 var fs = require('fs');
 var async = require('async');
+var express = require('express');
+var router = express.Router();
 module.exports= function(req, res, next){
 var obj = JSON.parse(fs.readFileSync('staffDetails.json', 'utf8'));
 
 async.each(obj, function(employee,callback){
-	if(employee.tpxID){
+
 		var userEntry = new User ({
 			tpxID : employee.tpxID.toUpperCase(),
 			passWord : 'Tesco123'
@@ -16,7 +18,12 @@ async.each(obj, function(employee,callback){
 				if (err){
 					console.log(err);
 				}
+		  else{
+				res.send('successfully updated the enteries');
+			}
 		});
-	}
+
 });
+
+
 }
