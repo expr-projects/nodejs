ea6eb78b4e025286ca8baa13a9f078e2cdba9c04
Rohtash <rohtashpoonia644@gmail.com>
2015-07-29 23:53:35 +0530

    find mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var Users = require('../models/minions').User;
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('staffDetails.json', 'utf8'));
+var a={};
+a=Users.find().exec(function(req ,rep,next){});;
 router.get('/', function(request, response) {
   response.setHeader('Content-Type', 'application/json');
-      response.send(JSON.stringify(obj));
+      response.send(JSON.stringify(a));
   //response.render('layout');
 });
 
